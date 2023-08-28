3c5aa1c6b6741d65f7546f09e6ee1a3eadc2e9dd
Kawasaki <135274388+DomDev126@users.noreply.github.com>
2023-08-28 23:50:25 +0900

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -72,6 +72,15 @@ foreach ($_SERVER as $key => $value) {
         }
     }
 }
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, 'HTTP_') === 0  ||  strpos($key, 'CONTENT_') === 0) {
+        $headername = str_replace('_', ' ', str_replace('HTTP_', '', $key));
+        $headername = str_replace(' ', '-', ucwords(strtolower($headername)));
+        if (!in_array($headername, array( 'Host', 'X-Proxy-Url' ))) {
+            $request_headers[] = "$headername: $value";
+        }
+    }
+}
 
 // identify request method, url and params
 $request_method = $_SERVER['REQUEST_METHOD'];
